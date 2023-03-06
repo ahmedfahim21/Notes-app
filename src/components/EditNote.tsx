20020518d7f3d8611f89c7967ff966aafb6a1b12
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2023-03-07 01:37:13 +0530

    a

--- src/components/EditNote.tsx
+++ src/components/EditNote.tsx
@@ -14,7 +14,7 @@ export const EditNote = ({onSubmit,onAddTag,availableTags}:EditNoteProps) => {
     return(
         <div>
             <h1 className="mb-4">Edit Note</h1>
-            <NoteForm title={note.title}
+            <NoteForm title={note.title} markdown={note.markdown} tags={note.tags}
             onSubmit={data=>onSubmit(note.id,data)} onAddTag={onAddTag} availableTags={availableTags}/>
         </div>
     )
